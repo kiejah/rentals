4571cfa6161231c112ae169c2474b8f311a835af
kiejah <kiejah@gmail.com>
2021-08-12 16:53:47 +0300

    Update database.php
    new db user password

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'db_rentals'),
             'username' => env('DB_USERNAME', 'rentals_user'),
-            'password' => env('DB_PASSWORD', '12345678'),
+            'password' => env('DB_PASSWORD', 'dd4edeba'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
